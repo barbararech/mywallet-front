cb9b8f75896041b55f5c53fd5f6b6a90eec1da00
barbararech <rech.brb@gmail.com>
2022-06-28 14:30:54 -0300

    feat: implement login page layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserContext from "../context/UserContext";
-import { useState } from 'react';
-import '../assets/styles/reset.css'
+import { useState } from "react";
+import "../assets/styles/reset.css";
+import GlobalStyle from "../assets/styles/globalStyles";
 
 import Login from "./Login";
-import SignUp from "./SignUp";
-import Transactions from "./Transactions";
-import Income from "./Income";
-import Expense from "./Expense";
+// import SignUp from "./SignUp";
+// import Transactions from "./Transactions";
+// import Income from "./Income";
+// import Expense from "./Expense";
 
 export default function App() {
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
-    return (
-        <UserContext.Provider value={{ user, setUser}}>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/signup" element={<SignUp />} />
                 <Route path="/transactions" element={<Transactions />} />
                 <Route path="/income" element={<Income />} />  
-                <Route path="/expense" element={<Expense />} />  
-            </Routes>
-        </BrowserRouter>
-        </UserContext.Provider>
-    );
+                <Route path="/expense" element={<Expense />} />   */}
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+import axios from "axios";
+import styled from "styled-components";
+
+export default function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  async function Login(event) {
+    event.preventDefault();
+
+    const body = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await axios.post("http://localhost:5000/login", body);
+      const { name, email, token } = response;
+
+      setUser({
+        name,
+        email,
+        token,
+      });
+
+      navigate("/transactions");
+    } catch (error) {
+      const message = error.response.statusText;
+
+      alert(message);
+    }
+  }
+
+  function LoginForm() {
+    return (
+      <>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          placeholder="E-mail"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          placeholder="Senha"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+      </>
+    );
+  }
+
+  return (
+    <Container>
+      <h1>My Wallet</h1>
+      <LoginForms onSubmit={Login}>{LoginForm()}</LoginForms>
+      <Link to="/signup">Primeira vez? Cadastre-se!</Link>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: "Lexend Deca", sans-serif;
+  height: 100vh;
+`;
+const LoginForms = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+`;

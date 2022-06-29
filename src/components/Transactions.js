78380b90623835ff488c9ee35b1b13ccbfa94070
barbararech <rech.brb@gmail.com>
2022-06-29 11:43:04 -0300

    feat: implement transactions page layout

---
+++ src/components/Transactions.js
@@ -0,0 +1,168 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../context/UserContext";
+import axios from "axios";
+import styled from "styled-components";
+import {
+  IoExitOutline,
+  IoAddCircleOutline,
+  IoRemoveCircleOutline,
+} from "react-icons/io5";
+
+export default function Signup() {
+  const { user } = useContext(UserContext);
+  const { name, token } = user;
+  console.log(token);
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    async function GetTransactions() {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/transactions",
+          config
+        );
+        console.log(response);
+
+        setTransactions(response.data);
+      } catch (error) {
+        const message = error.response.statusText;
+        alert(message);
+      }
+    }
+    GetTransactions();
+  }, []);
+
+  function renderTransactions() {
+    console.log(transactions);
+    if (transactions.length === 0) {
+      return <p>Não há registros de entrada ou saída</p>;
+    }
+
+    transactions.map((transaction) => {
+      const { date, description, value } = transactions;
+      return (
+        <>
+          <span>{date}</span>
+          <span>{description}</span>
+          <span>{value}</span>
+        </>
+      );
+    });
+  }
+
+  return (
+    <Container>
+      <TransactionsHeader>
+        <h2>Olá, {name}</h2>
+        <icon>
+          <IoExitOutline />
+        </icon>
+      </TransactionsHeader>
+      <TransactionsContainer>{renderTransactions()}</TransactionsContainer>
+      <ContainerAddTransaction>
+        <Link to="/income">
+          <icon>
+            <IoAddCircleOutline />
+          </icon>
+          <span>Nova Entrada</span>
+        </Link>
+        <Link to="/expense">
+          <icon>
+            <IoRemoveCircleOutline />
+          </icon>
+          <span>Nova Saída</span>
+        </Link>
+      </ContainerAddTransaction>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: "Raleway", sans-serif;
+  height: 100vh;
+  padding-bottom: 50px;
+
+  h2 {
+    font-weight: 700;
+    font-size: 26px;
+    color: #ffffff;
+    line-height: 30px;
+  }
+`;
+
+const TransactionsHeader = styled.div`
+  display: flex;
+  flex-direction: row-gap;
+  align-items: center;
+  justify-content: space-between;
+
+  icon {
+    font-size: 30px;
+    color: #ffffff;
+  }
+`;
+
+const TransactionsContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+  border-radius: 5px;
+  margin: 22px 0 13px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 15px;
+
+  p {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    font-family: "Raleway", sans-serif;
+    font-size: 20px;
+    color: #868686;
+    line-height: 24px;
+  }
+`;
+
+const ContainerAddTransaction = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    height: 115px;
+    width: 50%;
+    margin: 0;
+    border-radius: 5px;
+    background-color: #a328d6;
+    padding: 10px;
+    font-size: 17px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    word-spacing: 100vw;
+    line-height: 20px;
+  }
+
+  a:first-child {
+    margin-right: 8px;
+  }
+
+  icon {
+    font-size: 25px;
+    color: #ffffff;
+  }
+`;
